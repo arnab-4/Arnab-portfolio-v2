7d35223279c209a57c41344785126956c1f69854
Arnab Manna <107208391+arnab-4@users.noreply.github.com>
2023-09-13 23:31:03 +0530

    Update PreLoader.tsx

--- app/animations/PreLoader/PreLoader.tsx
+++ app/animations/PreLoader/PreLoader.tsx
@@ -13,7 +13,7 @@ const PreLoader = () => {
       <div className="texts-container w-500 flex h-60 items-center justify-center gap-[5px] overflow-hidden text-[14px] font-extrabold text-[#e4ded7] opacity-0 sm:gap-[10px] sm:text-[16px] md:text-[18px] lg:text-[20px]">
         <span>Developer/</span>
         <span>Designer/</span>
-        <span>Problem Solver.</span>
+        <span>Driller.</span>
         <div className="sub hidden"></div>
       </div>
     </div>
